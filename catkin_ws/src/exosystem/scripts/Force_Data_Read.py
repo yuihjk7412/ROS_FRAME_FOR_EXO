8bc16d1f67f5dd0e4ee287398ab99f8383d42fe5
yuihjk7412 <514522970@qq.com>
2019-07-30 23:35:53 +0800

    import force reading part without verify

---
+++ catkin_ws/src/exosystem/scripts/Force_Data_Read.py
@@ -0,0 +1,29 @@
+import serial
+import time
+import numpy as np
+import threading
+import binascii
+import rospy
+from std_msgs.msg import Int32
+
+
+if __name__ == '__main__':
+    port_Num = input('PLEASE INPUT THE PORT NUMBER(/dev/ttyUSB*):')
+    request_Command = bytes.fromhex('01 03 01 C2 00 08 E4 0C')
+    with serial.Serial("/dev/ttyUSB%d" % int(port_Num), 115200, timeout=0.2) as ser:
+        print("Serial Port OK!")
+    ser.close()
+    pub = rospy.Publisher('force_topic', Int32, queue_size=10)
+    rospy.init_node('force_talker', anonymous=True)
+    rate = rospy.Rate(50)
+    while not rospy.is_shutdown():
+        with serial.Serial("/dev/ttyUSB%d" % int(port_Num), 115200, timeout=0.2) as ser:
+            ser.write(request_Command)
+            buf = ser.read(21)
+
+        force_val = int.from_bytes(buf[3:7], signed=True, byteorder='big')
+        pub.publish(force_val)
+        rospy.loginfo("force:%d"%force_val)
+        #print(int.from_bytes(buf[3:7], signed=True, byteorder='big'))
+
+        rate.sleep()
