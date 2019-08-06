723dffbb46531e497ca2be88961e4410840e371b
yuihjk7412 <514522970@qq.com>
2019-08-06 23:41:57 +0800

    completesed

--- catkin_ws/src/exosystem/scripts/Encoder_read.py
+++ catkin_ws/src/exosystem/scripts/Encoder_read.py
@@ -5,18 +5,20 @@ import serial
 
 if __name__ == '__main__':
     port_Num = input('PLEASE INPUT THE PORT NUMBER(/dev/ttyUSB*):')
+    request_Command = bytes.fromhex('7ff7')
     with serial.Serial("/dev/ttyUSB%d" % int(port_Num), 115200, timeout=0.2) as ser:
         print("Serial Port OK!")
+    ser.close()
     pub = rospy.Publisher('encoder_topic', UInt16, queue_size=10)
     rospy.init_node('encoder_talker', anonymous=True)
     rate = rospy.Rate(50)
     while not rospy.is_shutdown():
-        with serial.Serial("/dev/ttyUSB%d" % int(port_Num), 115200, timeout=0.1) as ser:
+        with serial.Serial("/dev/ttyUSB%d" % int(port_Num), 115200, timeout=None) as ser:
+            ser.write(request_Command)
             buf = ser.read(6)
-            print(buf)
             if len(buf) < 6:
                 continue
-            if buf[0] == b'\x7f' and buf[-1] == b'\xf7':
+            if buf[0] == 0x7f and buf[-1] == 0xf7:
                 print("found data")
                 encoder1 = int.from_bytes(buf[1:3], signed=False, byteorder='big')
                 encoder2 = int.from_bytes(buf[3:5], signed=False, byteorder='big')
